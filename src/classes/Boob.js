039c9b1aead5e4131751f527defe4d836480b1e9
Derrien Nils <nils.derrien@etu.univ-lyon1.fr>
2025-07-02 14:50:33 +0200

    init avec mauvaise tech

---
+++ src/classes/Boob.js
@@ -0,0 +1,4 @@
+ // hello there
+ class Boob {
+
+ }
---
+++ vite.config.js
@@ -0,0 +1,5 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    root: 'src', // indique que le dossier 'src' est la racine du projet
+})
